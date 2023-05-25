fae5ab814e42e21308c435fcacba8164a57556aa
Prakhar Saraswat <prakhar.saraswat@geminisolutions.com>
2023-05-25 11:52:50 +0530

    First commit in Angular Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  paraValue = '';
+  parameter = true;
+  flag = true;
+  getDetails(flagType: boolean) {
+    if (this.parameter == true) {
+      this.paraValue = "Hi, I am Prakhar Saraswat";
+      this.parameter = false;
+    }
+    else {
+      this.paraValue = '';
+      this.parameter = true;
+    }
+  }
+  spanData = "";
+  count = 1;
+  addDetails(spanData: string) {
+    let span = document.getElementById('span');
+    let innerSpan = document.createElement('span');
+    innerSpan.innerHTML = " " + spanData;
+    span?.appendChild(innerSpan);
+    if (this.count % 5 == 0) {
+      innerSpan.style.backgroundColor = "blue";
+    }
+    this.count++;
+  }
 }
